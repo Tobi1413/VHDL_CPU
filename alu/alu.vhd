92d31eeef8431fa237040045dc0832a2ca3c90bc
Tobias Blumers <t.blumers@online.de>
2022-12-01 17:34:09 +0100

    alu shift verbessert

--- alu/alu.vhd
+++ alu/alu.vhd
@@ -38,7 +38,7 @@ begin
         result <= std_logic_vector(signed(input1) - signed(input2));
 
       when uSLL  => -- Shift Left Logical
-        result <= input1(wordwidth - 2 downto 0) & '0';
+        result <= std_logic_vector(unsigned(input1) sll to_integer(unsigned(input2(4 downto 0))));
         --(std_logic_vector(unsigned(input1)) sll 1);
 
       when uSLT  => -- Set on Less Than (Test if less than)
@@ -59,10 +59,11 @@ begin
         result <= (input1 xor input2);
 
       when uSRL  => -- Shift Right Logical      |a|b|c|d|   mit   >>   wird zu   |0|a|b|c|
-        result <= '0' & input1(wordwidth - 1 downto 1);
+        result <= std_logic_vector(unsigned(input1) srl to_integer(unsigned(input2(4 downto 0))));
+        --result <= input1 sll 3;
 
       when uSRA  => -- Shift Right Arithmetic   |a|b|c|d|   mit   >>   wird zu   |a|a|b|c|
-        result <= input1(wordwidth - 1) & input1(wordwidth - 1 downto 1);
+        result <= To_StdLogicVector(to_bitvector(input1) sra to_integer(unsigned(input2(4 downto 0))));
 
       when uOR   => -- OR
         result <= (input1 or input2);
